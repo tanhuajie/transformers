870b58c5365c354127a36b4dcfe6767fee0fade2
tanhuajie <tanhuajie264@163.com>
2025-09-26 03:01:15 +0800

    Add codes for llava-onevision-1.5

--- src/transformers/models/auto/video_processing_auto.py
+++ src/transformers/models/auto/video_processing_auto.py
@@ -52,6 +52,7 @@ else:
             ("internvl", "InternVLVideoProcessor"),
             ("llava_next_video", "LlavaNextVideoVideoProcessor"),
             ("llava_onevision", "LlavaOnevisionVideoProcessor"),
+            ("llava_onevision1_5", "Qwen2VLVideoProcessor"),
             ("perception_lm", "PerceptionLMVideoProcessor"),
             ("qwen2_5_omni", "Qwen2VLVideoProcessor"),
             ("qwen2_5_vl", "Qwen2VLVideoProcessor"),
